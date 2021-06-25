4f478eb4b8450276596ba3dc2b2be42460370e80
Farren-C <69100667+Farren-C@users.noreply.github.com>
2021-06-25 11:11:10 +0200

    Update collaborative_based.py

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -1,30 +1,21 @@
 """
-
     Collaborative-based filtering for item recommendation.
-
     Author: Explore Data Science Academy.
-
     Note:
     ---------------------------------------------------------------------
     Please follow the instructions provided within the README.md file
     located within the root of this repository for guidance on how to use
     this script correctly.
-
     NB: You are required to extend this baseline algorithm to enable more
     efficient and accurate computation of recommendations.
-
     !! You must not change the name and signature (arguments) of the
     prediction function, `collab_model` !!
-
     You must however change its contents (i.e. add your own collaborative
     filtering algorithm), as well as altering/adding any other functions
     as part of your improvement.
-
     ---------------------------------------------------------------------
-
     Description: Provided within this file is a baseline collaborative
     filtering algorithm for rating predictions on Movie data.
-
 """
 
 # Script dependencies
@@ -36,9 +27,6 @@ from surprise import Reader, Dataset
 from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
-#Additional librabries
-from scipy.sparse import csr_matrix
-from sklearn.neighbors import NearestNeighbors
 
 # Importing data
 movies_df = pd.read_csv('resources/data/movies.csv',sep = ',',delimiter=',')
@@ -95,63 +83,48 @@ def pred_movies(movie_list):
     # Return a list of user id's
     return id_store
 
-#Preprocessing for aditional algorithm
-
-#Remove timestamp from ratings data and get unique user values for each movie ID
-final_dataset = ratings_df.pivot(index='movieId',columns='userId',values='rating')
-
-#Impute Nan's
-final_dataset.fillna(0,inplace=True)
-
-#Aggregate the number of users who voted and the number of movies that were voted.
-no_user_voted = ratings_df.groupby('movieId')['rating'].agg('count')
-no_movies_voted = ratings_df.groupby('userId')['rating'].agg('count')
-
-#Set threshold for users and movies
-final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index,:]
-final_dataset = final_dataset.loc[:,no_movies_voted[no_movies_voted > 50].index]
-
-#Reduce sparsity in the data
-csr_data = csr_matrix(final_dataset.values)
-final_dataset.reset_index(inplace=True)
-
-#Build a recommender model
-knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-knn.fit(csr_data)
-
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
 def collab_model(movie_list,top_n=10):
     """Performs Collaborative filtering based upon a list of movies supplied
        by the app user.
-
     Parameters
     ----------
     movie_list : list (str)
         Favorite movies chosen by the app user.
     top_n : type
         Number of top recommendations to return to the user.
-
     Returns
     -------
     list (str)
         Titles of the top-n movie recommendations to the user.
-
     """
 
-    n_movies_to_reccomend = 10
-    user_movies = movies[movies['title'].str.contains(movie_list)]  
-    if len(user_movies):        
-        movie_idx= user_movies.iloc[0]['movieId']
-        movie_idx = final_dataset[final_dataset['movieId'] == movie_idx].index[0]
-        distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
-        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
-        recommend_frame = []
-        for val in rec_movie_indices:
-            movie_idx = final_dataset.iloc[val[0]]['movieId']
-            idx = movies[movies['movieId'] == movie_idx].index
-            recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0],'Distance':val[1]})
-        df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
-        return df
-    else:
-        return "No matches found. Please check your input or try a different movie"
+    indices = pd.Series(movies_df['title'])
+    movie_ids = pred_movies(movie_list)
+    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
+    for i in movie_ids :
+        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
+    # Getting the cosine similarity matrix
+    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
+    idx_1 = indices[indices == movie_list[0]].index[0]
+    idx_2 = indices[indices == movie_list[1]].index[0]
+    idx_3 = indices[indices == movie_list[2]].index[0]
+    # Creating a Series with the similarity scores in descending order
+    rank_1 = cosine_sim[idx_1]
+    rank_2 = cosine_sim[idx_2]
+    rank_3 = cosine_sim[idx_3]
+    # Calculating the scores
+    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
+    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
+    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
+     # Appending the names of movies
+    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    recommended_movies = []
+    # Choose top 50
+    top_50_indexes = list(listings.iloc[1:50].index)
+    # Removing chosen movies
+    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+    for i in top_indexes[:top_n]:
+        recommended_movies.append(list(movies_df['title'])[i])
+    return recommended_movies
